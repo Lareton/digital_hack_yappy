5678a7519bd228e1e7ce8c29ddf4ded63e2fefbf
andrey-blight <kizhinov05@gmail.com>
2024-09-29 01:55:30 +0300

    fix docker

--- ml_algo/main.py
+++ ml_algo/main.py
@@ -1,14 +1,12 @@
+from algo_utils import download_video
 from ml_algo import get_res_by_uuid
 from check_duplicate import model, df, vector_db
-print("All imports")
+
 
 def check_video_is_duplicate_by_uuid(uuid_video):
     video_link = f"https://s3.ritm.media/yappy-db-duplicates/{uuid_video}.mp4"
-    print("first")
-#    download_video(video_link, f"{uuid_video}.mp4")
-    print("second")
+    download_video(video_link, f"{uuid_video}.mp4")
     pred_is_dup, pred_uuid = get_res_by_uuid(df, vector_db, model, uuid_video, 0.3)
-    print("3")
     return pred_is_dup, pred_uuid
 
 
